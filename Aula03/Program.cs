186019c22680cbd2b2774f77300641810acc4cb1
Gabriel <agentecpf222222222@gmail.com>
2026-09-05 04:55:45 -0300

    IO, Const, Var

--- Aula03/Program.cs
+++ Aula03/Program.cs
@@ -3,7 +3,14 @@ public class Program
 {
     public static void Main()
     {
-        string nome = "Gabriel";
-        Console.WriteLine(nome);
+        int senha = 2004;
+        Console.WriteLine("Seu Nome: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Olá "+ nome);
+        Console.WriteLine("Qual Sua senha?");
+        int tentativa = Convert.ToInt32(Console.ReadLine());
+        if (tentativa == senha)
+            Console.WriteLine("Passou");
+
     }
 }
