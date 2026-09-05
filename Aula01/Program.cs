9436cf1224a00e14608d01804f78ef7062c18d48
Gabriel <agentecpf222222222@gmail.com>
2026-09-05 04:10:38 -0300

    CLI, E base para criar um progama em C#

---
+++ Aula01/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Aula01;
+public class Progam
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hey");
+    }
+}
---
+++ Aula02/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Aula02;
+public class Progam
+{
+    public static void Main()
+    {
+        Console.WriteLine("UP");
+    }
+}
